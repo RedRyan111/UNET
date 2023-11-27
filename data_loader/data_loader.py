7830d472f5b44228e4f9bef8adc8920d35b1a75e
RedRyan111 <ravenengineerryan@gmail.com>
2023-11-27 10:32:44 -0600

    Fixed train data loader

--- data_loader/data_loader.py
+++ data_loader/data_loader.py
@@ -3,42 +3,33 @@ import torch
 from PIL import Image
 from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms, utils
+import matplotlib.pyplot as plt
 import os
-
-# im = Image.open(r"carvana-image-masking-challenge\train\train\0cdf5b5d0ce1_01.jpg")
-# im.show()
-
-print(f"inside data loader: {os.getcwd()}")
-
-mask = Image.open(r"carvana-image-masking-challenge/train_masks/train_masks/0cdf5b5d0ce1_01_mask.gif")
-mask = np.array(mask.convert("L"), dtype=np.float32)
-mask = torch.tensor(mask)
-
-print(mask.shape)
 
 
 class CustomImageDataset(Dataset):
-    def __init__(self, annotations_file, img_dir, transform=None, target_transform=None):
+    def __init__(self, transform=None, target_transform=None):
         self.train_images_dir_name = "carvana-image-masking-challenge/train/train/"
         self.train_masks_dir_name = "carvana-image-masking-challenge/train_masks/train_masks/"
 
-        self.train_images_file_name_list = os.listdir(train_images_dir_name)
-        self.train_masks_file_name_list = os.listdir(train_masks_dir_name)
+        self.train_images_file_name_list = os.listdir(self.train_images_dir_name)
+        self.train_masks_file_name_list = os.listdir(self.train_masks_dir_name)
 
         self.transform = transform
         self.target_transform = target_transform
 
     def __len__(self):
-        return len(self.train_images_dir_name)
+        return len(self.train_images_file_name_list)
 
     def get_image(self, idx):
-        file_name = self.train_images_file_name_list[idx]
+        file_name = self.train_images_dir_name + self.train_images_file_name_list[idx]
         image = Image.open(file_name)
+        image = np.array(image.convert("L"), dtype=np.float32)
         image_tensor = torch.tensor(image)
         return image_tensor
 
     def get_mask(self, idx):
-        file_name = self.train_masks_file_name_list[idx]
+        file_name = self.train_masks_dir_name + self.train_masks_file_name_list[idx]
         mask = Image.open(file_name)
         mask = np.array(mask.convert("L"), dtype=np.float32)
         mask = torch.tensor(mask)
@@ -56,10 +47,12 @@ class CustomImageDataset(Dataset):
         return image, mask
 
 
-train_images_dir_name = "carvana-image-masking-challenge/train/train/"
-train_masks_dir_name = "carvana-image-masking-challenge/train_masks/train_masks/"
+TrainingCustomImageDataset = CustomImageDataset()
+print(len(TrainingCustomImageDataset))
 
-train_images_file_name_list = os.listdir(train_images_dir_name)
-train_masks_file_name_list = os.listdir(train_masks_dir_name)
-print(len(train_images_file_name_list))
-print(len(train_masks_file_name_list))
+# Display image and label.
+train_dataloader = DataLoader(TrainingCustomImageDataset, batch_size=64, shuffle=True)
+train_features, train_labels = next(iter(train_dataloader))
+print(f"Feature batch shape: {train_features.size()}")
+print(f"Labels batch shape: {train_labels.size()}")
+
