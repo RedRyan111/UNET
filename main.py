e49646b1b1518c408dcc57788f5b069cc29076b5
RedRyan111 <ravenengineerryan@gmail.com>
2023-12-06 00:28:50 -0600

    Completed UNET trianability

--- main.py
+++ main.py
@@ -5,24 +5,35 @@ from torch.utils.data import DataLoader
 from data_loader.data_loader import CustomImageDataset
 from setup import batch_size
 import models.UNET as UNET
+import torch.nn as nn
+import torch.optim as op
 
 TrainingCustomImageDataset = CustomImageDataset()
 print(len(TrainingCustomImageDataset))
 
 # Display image and label.
 train_dataloader = DataLoader(TrainingCustomImageDataset, batch_size=batch_size, shuffle=True)
-train_features, train_labels = next(iter(train_dataloader))
-print(f"Feature batch shape: {train_features.size()}")
-print(f"Labels batch shape: {train_labels.size()}")
+features = [12, 24, 48]
+num_inp_channels = 3
+num_labels = 1
+model = UNET.UNET(features, num_inp_channels, num_labels)
 
-train_features = train_features.permute(0, 3, 2, 1)
+mse_loss = nn.MSELoss()
+optimizer = op.Adam(model.parameters(), lr=.001)
 
-max_pool = torch.nn.MaxPool2d(kernel_size=(3, 3))
-train_features = max_pool(train_features)
+epochs = 100
 
-print(f"Max pooled features: {train_features.size()}")
+for epoch in range(epochs):
+    train_features, train_labels = next(iter(train_dataloader))
+    train_features = train_features.permute(0, 3, 2, 1)
 
-features = [3, 64, 64]
-model = UNET.UNET()
-outs = model(train_features)
-print(f'out shape: {outs.shape}')
+    outs = model(train_features)
+
+    outs = outs.permute(0, 3, 2, 1).squeeze()
+
+    loss = mse_loss(train_labels, outs)
+    loss.backward()
+    optimizer.step()
+    print(f'epoch: {epoch} loss: {loss}')
+
+print('DONE!')
--- models/UNET.py
+++ models/UNET.py
@@ -8,27 +8,26 @@ import torch.nn.functional as F
 import torchvision.transforms.functional as TF
 
 
-class DoubleConv(nn.Module):
-    def __init__(self, features):
-        super(DoubleConv, self).__init__()
+class UNET(nn.Module):
+    def __init__(self, features, num_inp_channels, num_labels):
+        super(UNET, self).__init__()
 
-        self.conv = nn.Sequential(
-            nn.Conv2d(features[0], features[1], 3, 1, 1, bias=False),
-            nn.BatchNorm2d(features[1]),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(features[1], features[2], 3, 1, 1, bias=False),
-            nn.BatchNorm2d(features[2]),
-            nn.ReLU(inplace=True)
-        )
+        self.features = features#[3, 12, 24, 48]#[3, 64, 128, 256, 512, 1024]
+
+        self.encoder = Encoder([num_inp_channels] + self.features)
+        self.decoder = Decoder([num_labels] + self.features)
 
     def forward(self, x):
-        return self.conv(x)
+        out_list = self.encoder(x)
+        y = self.decoder(out_list)
+        y = TF.resize(y, size=x.shape[2:])
+        return y
 
 
 class Encoder(nn.Module):
-    def __init__(self, features, down_sampler):
+    def __init__(self, features):
         super(Encoder, self).__init__()
-        self.down_sampler = down_sampler
+        self.down_sampler = DownSample(scaling_factor=2)
         self.module = nn.ModuleList([])
         self.setup_module(features)
 
@@ -36,7 +35,6 @@ class Encoder(nn.Module):
         feature_length = len(features)
         for i in range(feature_length-1):
             double_conv_features = [features[i], features[i+1], features[i+1]]
-            print(f'encoder features: {double_conv_features}')
             double_conv = DoubleConv(double_conv_features)
             self.module.append(double_conv)
 
@@ -45,12 +43,11 @@ class Encoder(nn.Module):
         cur_out = x
         for module in self.module:
             cur_out = module(cur_out)
+
             h = cur_out.shape[2] + cur_out.shape[2] % 2
             w = cur_out.shape[3] + cur_out.shape[3] % 2
             padded_out = TF.resize(cur_out, size=[h, w])
 
-            #padded_out = pad_tensor_shapes_if_odd(cur_out)
-            #print(f'prev out: {cur_out.shape} padded out: {padded_out.shape}')
             out_list.append(padded_out)
 
             cur_out = self.down_sampler(padded_out)
@@ -71,7 +68,6 @@ class Decoder(nn.Module):
     def setup_double_conv(self, features):
         for i in range(len(features)-1):
             double_conv_features = [features[i], features[i+1], features[i+1]]
-            print(f'decoder features: {double_conv_features}')
             double_conv = DoubleConv(double_conv_features)
             self.module.append(double_conv)
 
@@ -86,7 +82,6 @@ class Decoder(nn.Module):
         cur_up_sample = up_sample_model(out_list[0])
         for i in range(len(out_list)-1):
             next_encoder_output = out_list[i+1]
-            #cur_up_sample = crop_tensor_to_tensor_h_and_w(cur_up_sample, next_encoder_output)
             cur_up_sample = TF.resize(cur_up_sample, next_encoder_output.shape[2:])
 
             double_conv_inp = torch.concatenate((cur_up_sample, next_encoder_output), dim=1)
@@ -97,40 +92,28 @@ class Decoder(nn.Module):
             if i == len(out_list)-2:
                 double_conv = self.module[i+1]
                 cur_double_conv = double_conv(cur_double_conv)
-                cur_double_conv = crop_tensor_to_tensor_h_and_w(cur_double_conv, next_encoder_output)
+                cur_double_conv = TF.resize(cur_double_conv, next_encoder_output.shape[2:])
                 return cur_double_conv
 
             up_sample_model = self.up_sampler[i+1]
-            cur_up_sample = up_sample_model(cur_double_conv) #this is causing problems
+            cur_up_sample = up_sample_model(cur_double_conv)
 
 
-class UNET(nn.Module):
-    def __init__(self):
-        super(UNET, self).__init__()
+class DoubleConv(nn.Module):
+    def __init__(self, features):
+        super(DoubleConv, self).__init__()
 
-        self.features = [3, 12, 24, 48]#[3, 64, 128, 256, 512, 1024]
-
-        self.down_sample = DownSample(scaling_factor=2)
-
-        self.encoder = Encoder(self.features, self.down_sample)
-        self.decoder = Decoder(self.features)
+        self.conv = nn.Sequential(
+            nn.Conv2d(features[0], features[1], 3, 1, 1, bias=False),
+            nn.BatchNorm2d(features[1]),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(features[1], features[2], 3, 1, 1, bias=False),
+            nn.BatchNorm2d(features[2]),
+            nn.ReLU(inplace=True)
+        )
 
     def forward(self, x):
-        out_list = self.encoder(x)
-        y = self.decoder(out_list)
-        y = crop_tensor_to_tensor_h_and_w(y, x)
-        return y
-
-
-def crop_tensor_to_tensor_h_and_w(x, y):
-    return torchvision.transforms.functional.center_crop(img=x, output_size=[y.shape[2], y.shape[3]])
-
-
-def pad_tensor_shapes_if_odd(inp_tensor):
-    tensor_shape = [0 for _ in range(4)]
-    tensor_shape[2] = inp_tensor.shape[2] % 2
-    tensor_shape[0] = inp_tensor.shape[3] % 2
-    return F.pad(inp_tensor, tensor_shape, "constant", 0)
+        return self.conv(x)
 
 
 class DownSample(nn.Module):
@@ -153,5 +136,4 @@ class UpSample(nn.Module):
     def forward(self, x):
         up = self.up_sample(x)
         conv = self.conv(up)
-        print(f'inp: {x.shape} up: {up.shape} conv: {conv.shape}')
         return conv
